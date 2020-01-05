70c212a8cb3d04886d3ed1c1f7eff7800d78b7f2
Your Name <1809252506@qq.com>
2020-01-05 23:59:42 +0800

    update

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -7,7 +7,7 @@
       :duration="duration"
       style="height:200px"
     >
-    <block v-for="item in imgUrls" :key="item">
+     <block v-for="item in imgUrls" :key="item">
       <swiper-item>
         <image :src="item" style="width:100%;"/>
       </swiper-item>
@@ -25,8 +25,8 @@
  <i-panel :title="title_name">
     <view style="padding: 15px;">
       <i-card v-for="item in top" :key="item" i-class="split"  :extra="item.name" :thumb="item.image">
-         <view slot="content">{{item.remark}}</view>
-         <view slot="footer">{{item.address}}</view>
+          <view slot="content">{{item.remark}}</view>
+          <view slot="footer">{{item.address}}</view>
     
     </i-card>
    </view>
